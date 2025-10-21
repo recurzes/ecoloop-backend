5c1b54a5b747133efa9a3a34ff7cba950df95099
Wasureta <forstakenajax@gmail.com>
2025-10-21 22:42:36 +0800

    new

--- app/api/auth.py
+++ app/api/auth.py
@@ -4,7 +4,8 @@ from jose import JWTError, jwt
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 
 
@@ -18,14 +19,24 @@ router = APIRouter(
 #dont know what im using hex 32?
 SECRET_KEY = "d7f3c1b6e5274a8d9bd5804f75d4854edb752a52138c8b5d3b1a2e27e09ff484"
 ALGORITHM = "HS256"
-bycrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
-
-class CreateUserRequest(BaseModel):
+class UserCreate(BaseModel):
     username: str
-    email: str
+    email: EmailStr
+    password: str
+    user_type: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
     password: str
 
-class Token(BaseModel):
+class TokenResponse(BaseModel):
     access_token: str
-    token_type: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+class ForgotPassword(BaseModel):
+    email: EmailStr
+
+class ResetPassword(BaseModel):
+    token: str
+    new_password: str
